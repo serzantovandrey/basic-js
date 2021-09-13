87e58921118a71616e40284e9cbfa3af7ee60d9b
andrey <andrey.serzantau@gmail.com>
2021-09-13 11:54:11 +0300

    feat: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -16,5 +16,16 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default function countCats(/* matrix */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let hears = 0;
+
+    for(let i of arr){
+       for(let j of i){
+          if(j == '^^'){
+             hears++;
+          }
+       }
+    }
+
+    return hears;
 }
+
